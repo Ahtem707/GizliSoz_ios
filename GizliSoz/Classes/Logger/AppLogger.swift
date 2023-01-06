9447aed21db0cad1f96af3b842f1528af9cafb3d
Ahtem707 <sitzhalilov.a.i.2.19@gmail.com>
2023-01-07 00:02:48 +0300

    Правка логов для версии ios ниже 14

--- GizliSoz/Classes/Logger/AppLogger.swift
+++ GizliSoz/Classes/Logger/AppLogger.swift
@@ -18,41 +18,75 @@ final class AppLogger {
     static func log(_ type: LogType, _ message: Any) {
         guard BuildConfig.loggingEnabled && Self.enableLogType.contains(type) else { return }
         guard let message = message as? String else { return }
-        let log = Logger(
-            subsystem: Bundle.main.bundleIdentifier!,
-            category: String(describing: type.rawValue)
-        )
-        log.notice("\(message, privacy: .public)")
+        if #available(iOS 14.0, *) {
+            let log = Logger(
+                subsystem: Bundle.main.bundleIdentifier!,
+                category: String(describing: type.rawValue)
+            )
+            log.notice("\(message, privacy: .public)")
+        } else {
+            let log = OSLog(
+                subsystem: Bundle.main.bundleIdentifier!,
+                category: String(describing: type.rawValue)
+            )
+            os_log("%@", log: log, type: .info, message)
+        }
     }
     
     static func warning(_ type: LogType, _ message: Any) {
         guard BuildConfig.loggingEnabled && Self.enableLogType.contains(type) else { return }
         guard let message = message as? String else { return }
-        let log = Logger(
-            subsystem: Bundle.main.bundleIdentifier!,
-            category: String(describing: type.rawValue)
-        )
-        log.warning("\(message, privacy: .public)")
+        
+        if #available(iOS 14.0, *) {
+            let log = Logger(
+                subsystem: Bundle.main.bundleIdentifier!,
+                category: String(describing: type.rawValue)
+            )
+            log.warning("\(message, privacy: .public)")
+        } else {
+            let log = OSLog(
+                subsystem: Bundle.main.bundleIdentifier!,
+                category: String(describing: type.rawValue)
+            )
+            os_log("%@", log: log, type: .fault, message)
+        }
     }
     
     static func error(_ type: LogType, _ message: Any) {
         guard BuildConfig.loggingEnabled && Self.enableLogType.contains(type) else { return }
         guard let message = message as? String else { return }
-        let log = Logger(
-            subsystem: Bundle.main.bundleIdentifier!,
-            category: String(describing: type.rawValue)
-        )
-        log.error("\(message, privacy: .public)")
+        if #available(iOS 14.0, *) {
+            let log = Logger(
+                subsystem: Bundle.main.bundleIdentifier!,
+                category: String(describing: type.rawValue)
+            )
+            log.error("\(message, privacy: .public)")
+        } else {
+            let log = OSLog(
+                subsystem: Bundle.main.bundleIdentifier!,
+                category: String(describing: type.rawValue)
+            )
+            os_log("%@", log: log, type: .error, message)
+        }
     }
     
     static func critical(_ type: LogType, _ message: Any) {
         guard BuildConfig.loggingEnabled && Self.enableLogType.contains(type) else { return }
         guard let message = message as? String else { return }
-        let log = Logger(
-            subsystem: Bundle.main.bundleIdentifier!,
-            category: String(describing: type.rawValue)
-        )
-        log.critical("\(message, privacy: .public)")
+        
+        if #available(iOS 14.0, *) {
+            let log = Logger(
+                subsystem: Bundle.main.bundleIdentifier!,
+                category: String(describing: type.rawValue)
+            )
+            log.critical("\(message, privacy: .public)")
+        } else {
+            let log = OSLog(
+                subsystem: Bundle.main.bundleIdentifier!,
+                category: String(describing: type.rawValue)
+            )
+            os_log("%@", log: log, type: .error, message)
+        }
     }
 }
 
