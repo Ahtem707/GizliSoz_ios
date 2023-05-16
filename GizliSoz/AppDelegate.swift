dde92fcef124d23b38d3cc937d9202bb198dfcc3
Ahtem Sitjalilov <a.sitjalilov@ideas-world.com>
2023-05-16 19:57:39 +0300

    Заблокировал поворот экрана и выставил только портретную ориентацию

--- GizliSoz/AppDelegate.swift
+++ GizliSoz/AppDelegate.swift
@@ -27,4 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
